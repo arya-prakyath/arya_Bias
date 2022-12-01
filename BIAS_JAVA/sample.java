2ee9e8dfe3c588c86dcd549fd24ac94615364d24
Prakyath Arya <Arya@ussilica.com>
2022-12-01 14:05:35 +0530

    Init

---
+++ BIAS_JAVA/sample.java
@@ -0,0 +1,9 @@
+public class sample{
+    public static class Sample{
+        int a=20;
+    }
+
+    public static void main(String[] args){
+        System.out.println(args[0] + args[1]);
+    }
+}
